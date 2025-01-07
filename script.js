465bb687d75d00e811c8e222dfbf21faaaa20c7d
Marcel-Lukas <m.lukas@mailbox.org>
2025-01-07 21:10:03 +0100

    firebase url

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const BASE_URL = "https://join-b35b0-default-rtdb.europe-west1.firebasedatabase.app/";
+const BASE_URL = "https://join-marcel-lukas-default-rtdb.europe-west1.firebasedatabase.app/";
 
