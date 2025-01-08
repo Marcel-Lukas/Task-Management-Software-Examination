3f7b753ac487313afa344fab8fc899e8cd839ece
Marcel-Lukas <m.lukas@mailbox.org>
2025-01-08 13:00:45 +0100

    Navigation Template

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+
+async function includeHTML() {
+  let includeElements = document.querySelectorAll('[w3-include-html]');
+  for (let i = 0; i < includeElements.length; i++) {
+      const element = includeElements[i];
+      let file = element.getAttribute("w3-include-html");
+      let resp = await fetch(file);
+      if (resp.ok) {
+          element.innerHTML = await resp.text();
+      } else {
+          element.innerHTML = 'Page not found';
+      }
+  }
+}
+
+
+function extractFilename(url) {
+  const urlObj = new URL(url);
+  const pathname = urlObj.pathname;
+  const filenameWithExtension = pathname.substring(pathname.lastIndexOf('/') + 1);
+  const filename = filenameWithExtension.split('.').slice(0, -1).join('.');
+  return filename;
+}
+
+
+function activeLink() {
+  setTimeout(() => {
+    const url = window.location.href;
+    const filename = extractFilename(url);
+    if(document.querySelector(`#${filename}`)) {
+        document.querySelector(`#${filename}`).classList.add('active');
+    }
+  }, 150);
+}
+
+activeLink();
