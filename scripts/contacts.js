db21b1190a620f8bccd35391fd2ba364993534c2
Marcel-Lukas <m.lukas@mailbox.org>
2025-01-11 13:54:20 +0100

    contacts.js fetchContacts()

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,6 +1,25 @@
+// async function fetchContacts() {
+//     let response = await fetch('https://join-marcel-lukas-default-rtdb.europe-west1.firebasedatabase.app/');
+//     let responseAsJson = await response.json();
+//     console.log(responseAsJson);
+
+// }
+
+
+
 async function fetchContacts() {
-    let response = await fetch('https://join-marcel-lukas-default-rtdb.europe-west1.firebasedatabase.app/');
-    let responseAsJson = await response.json();
-    console.log(responseAsJson);
-    
+  try {
+    let response = await fetch(`https://join-marcel-lukas-default-rtdb.europe-west1.firebasedatabase.app/contacts/.json`);
+    let text = await response.text(); // Rohdaten als Text
+    console.log("Rohe Antwort:", text);
+
+    let datas = JSON.parse(text);
+    console.log(datas);
+
+  } catch (error) {
+    console.error("Fehler:", error);
+  }
 }
+
+fetchContacts();
+
