05734bf76a2bcbbc80c6d540b0bd79fe1b62c87c
Marcel-Lukas <m.lukas@mailbox.org>
2025-01-16 17:11:33 +0100

    Lebt ihr beiden noch?!

--- scripts/accessEvents.js
+++ scripts/accessEvents.js
@@ -1,3 +1,5 @@
+// Für die Join Logo Animation
+
 document.addEventListener("DOMContentLoaded", () => {
   let logoContainer = document.querySelector(".logo-container");
   let logo = document.querySelector(".img-logo");
@@ -13,3 +15,34 @@ document.addEventListener("DOMContentLoaded", () => {
   }, 1500);
 
 });
+
+
+
+// Funktion um die Sichtbarkeit zwischen Anmelde- 
+// und Registrierungsfenster umzuschalten.
+
+function toggleAccessWindow() {
+  let logIn = document.getElementById("loginSection");
+  let signUp = document.getElementById("signUpSection");
+
+  logIn.classList.toggle("d-none");
+  signUp.classList.toggle("d-none");
+}
+
+
+
+// Wenn das Bild aktuell falsePath ist, auf 
+// truePath setzen, sonst zurück zu falsePath
+// Für Remember me & I accept the Legal notice
+
+function toggleCheckbox(imageId) {
+  const img = document.getElementById(imageId);
+  const falsePath = './assets/img/png/check-button-false.png';
+  const truePath  = './assets/img/png/check-button-true.png';
+
+  if (img.src.includes("check-button-false.png")) {
+    img.src = truePath;
+  } else {
+    img.src = falsePath;
+  }
+}
