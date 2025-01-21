a8cbe844f9804dfa3ede54ad7bdc6f6d35286deb
Marcel-Lukas <m.lukas@mailbox.org>
2025-01-21 21:21:23 +0100

    Matrix Style :P

--- cloud.js
+++ cloud.js
@@ -1,5 +1,5 @@
 function randomText(){
-    var text = ("JOIN")
+    var text = ("ア イ ウ エ オ カ キ ク ケ コ サ シ ス セ ソ タ チ ツ テ ト ナ ニ ヌ ネ ノ ハ ヒ フ ヘ ホ マ ミ ム メ モ ヤ ユ ヨ ラ リ ル レ ロ ワ ヲ ン ガ ギ グ ゲ ゴ ザ ジ ズ ゼ ゾ ダ ヂ ヅ デ ド バ ビ ブ ベ ボ パ ピ プ ペ ポ")
     letters =text[Math.floor(Math.random() * text.length)];
     return letters;
 }
@@ -11,15 +11,15 @@ function rain() {
     cloud.appendChild(e);
 
     let left = Math.floor(Math.random() * 300);
-    let size = Math.random() * 1.9;
+    let size = Math.random() * 0.9;
     let duration = Math.random() * 2;
 
     e.innerText = randomText();
     e.style.left = left + 'px';
     e.style.fontSize = 2 + size + 'em';
     e.style.animationDuration = 2 + duration + 's';
-    e.style.color = 'rgb(41, 171, 226)';
-    e.style.filter = 'drop-shadow(0 0 10px rgba(41, 170, 226, 0.8))'; 
+    e.style.color = 'rgb(62, 226, 41)';
+    e.style.filter = 'drop-shadow(0 0 10px rgba(21, 214, 31, 0.8))'; 
 
     setTimeout(function(){
         cloud.removeChild(e);
@@ -28,5 +28,5 @@ function rain() {
 
 setInterval(function(){
     rain()
-},222);
+},77);
 
