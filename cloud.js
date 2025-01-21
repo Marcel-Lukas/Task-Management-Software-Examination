e11c5bacd81061ae0863bcd0acbd9b131b92922f
Marcel-Lukas <m.lukas@mailbox.org>
2025-01-21 22:37:35 +0100

    The Matrix has you

--- cloud.js
+++ cloud.js
@@ -1,5 +1,5 @@
 function randomText(){
-    var text = ("ア イ ウ エ オ カ キ ク ケ コ サ  セ  タ チ  テ ト ナ  ヌ ネ ノ ハ ヒ フ ヘ ホ マ ミ ム メ モ ヤ ユ ヨ ラ リ ル レ ロ ワ ヲ ン ガ ギ グ ゲ ゴ ザ  ズ ゼ ゾ ダ ヂ  デ ド バ ビ ブ ベ  パ ピ プ ペ ")
+    let text = ("1010101011010101MATRIXアイウエカキクケコセタトナヌネハヒフヘマミムメモヤユヨラリルレロワヲングゴズゼゾダデドバビブベパピプペ")
     letters =text[Math.floor(Math.random() * text.length)];
     return letters;
 }
@@ -12,21 +12,21 @@ function rain() {
 
     let left = Math.floor(Math.random() * 600);
     let size = Math.random() * 0.8;
-    let duration = Math.random() * 1.2;
+    let duration = Math.random() * 4;
 
     e.innerText = randomText();
     e.style.left = left + 'px';
-    e.style.fontSize = 1.5 + size + 'em';
-    e.style.animationDuration = 1777 + duration + 'ms';
-    e.style.color = 'rgba(62, 226, 41, 0.8)';
-    e.style.filter = 'drop-shadow(0 0 10px rgba(21, 214, 31, 0.8))'; 
+    e.style.fontSize = 1.1 + size + 'em';
+    e.style.animationDuration = 1555 + duration + 'ms';
+    e.style.color = 'rgba(62, 226, 41, 0.9)';
+    e.style.filter = 'drop-shadow(0 0 10px rgba(21, 214, 31, 0.7))'; 
 
     setTimeout(function(){
         cloud.removeChild(e);
-    }, 2000);
+    }, 2444);
 }
 
 setInterval(function(){
     rain()
-},66);
+},33);
 
